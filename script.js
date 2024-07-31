9213f17b42d499fcf1c17503d42c0a50a7ed2d2f
Prateek Maddheshiya <prateekkumar247247@gmail.com>
2024-07-31 08:24:24 +0530

    js console api

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// learnt js in terminal
+// document.querySelector(".anyclass")
+// document.getElementById("name-of-that-id")
+// document.getElementById("name-of-that-id").click()
+// document.getElementsByTagName("h1")
+// console.clear
+// console.assert(4==(6-2))
+
+// 1.ways to print msg
 alert("anymsg to print")
+
+// 2.javascript console api
 console.log("any msg to print")
-document.querySelector(".anyclass")
+console.warn("this is warningg")
+console.error("this is an errorr")
