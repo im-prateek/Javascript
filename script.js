250d7ed12f094907f77b6e5749671650de8e8c3c
Prateek Maddheshiya <prateekkumar247247@gmail.com>
2024-08-01 15:14:16 +0530

    dataypes in js

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
-// document.querySelector(".anyclass")
-// document.getElementById("name-of-that-id")
-// document.getElementById("name-of-that-id").click()
-// document.getElementsByTagName("h1")
-// console.clear
-// console.assert(4==(6-2))
+/*
+document.querySelector(".anyclass")
+document.getElementById("name-of-that-id")
+document.getElementById("name-of-that-id").click()
+document.getElementsByTagName("h1")
+console.clear
+console.assert(4==(6-2))
 
-// 1.ways to print msg
+1.ways to print msg
 alert("anymsg to print")
 
-// 2.javascript console api
+2.javascript console api
 console.log("any msg to print")
 console.warn("this is warningg")
 console.error("this is an errorr")
+
+*/
+
+// 3. Javascript variables
+
+var number1=34;
+var number2=46;
+console.log(number1 + number2);
+
+// Datatypes in javascript
+var num1=234;
+var string1='this is a string'
+
+// object datatype
+var marks = {
+    aman:54,
+    harry:28,
+    arun:78
+}
+console.log(marks);
+
+// var und = undefined;
+var und;
+console.log(und);
+
+var n = null;
+console.log(n);
+
