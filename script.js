f5e22b5df5f53cc715ad039d9eac319a6245cafe
Prateek Maddheshiya <prateekkumar247247@gmail.com>
2024-08-21 19:37:07 +0530

    arrow function, settimeout, setinterval, clear timeout

--- script.js
+++ script.js
@@ -202,3 +202,20 @@ firstcontainer.addEventlistener('mouseup', function()){
 
 
 */
+
+// arrow function
+function sum(a, b){
+    return a+b;
+}
+
+sum = (a,b)=> {
+    return a+b;
+}
+
+// settimeout and setinterval
+logkaro = ()=>{
+    console.log('i am your log')
+}
+settimeout(logkaro, 2000);
+// use clearinterval/cleartimeout to cancel setinterval/settimeout
+clr = setinterval(logkaro, 2000);
